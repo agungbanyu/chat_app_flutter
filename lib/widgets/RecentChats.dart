e9026bed790bc4567712371bf1d37a44df8f8408
Alfarobi <alfarobi.dev@gmail.com>
2020-05-26 17:13:38 +0700

    https://youtu.be/h-igXZCCrrc?t=2270

--- lib/widgets/RecentChats.dart
+++ lib/widgets/RecentChats.dart
@@ -14,27 +14,77 @@ class RecentChats extends StatelessWidget {
             topRight: Radius.circular(30),
           ),
         ),
-        child: ListView.builder(
-          itemCount: chats.length,
-          itemBuilder: (BuildContext context, int index) {
-            final MessageModel chat = chats[index];
-            return Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
+        child: ClipRRect(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(30),
+            topRight: Radius.circular(30),
+          ),
+          child: ListView.builder(
+            itemCount: chats.length,
+            itemBuilder: (BuildContext context, int index) {
+              final MessageModel chat = chats[index];
+              return Container(
+                margin: EdgeInsets.only(
+                  top: 5,
+                  bottom: 5,
+                  right: 20,
+                ),
+                padding: EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 10,
+                ),
+                decoration: BoxDecoration(
+                    color: chat.unread ? Color(0xFFFFEFEE) : Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(20),
+                      bottomRight: Radius.circular(20),
+                    )),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    CircleAvatar(
-                      radius: 35,
-                      backgroundImage: AssetImage(chat.sender.imageUrl),
-                    ),
-                    SizedBox(
-                      width: 10,
+                    Row(
+                      children: [
+                        CircleAvatar(
+                          radius: 35,
+                          backgroundImage: AssetImage(chat.sender.imageUrl),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              chat.sender.name,
+                              style: TextStyle(
+                                color: Colors.grey,
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 5,
+                            ),
+                            Container(
+                              width: MediaQuery.of(context).size.width * 0.45,
+                              child: Text(
+                                chat.text,
+                                style: TextStyle(
+                                  color: Colors.blueGrey,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
                     Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Text(
-                          chat.sender.name,
+                          chat.time,
                           style: TextStyle(
                             color: Colors.grey,
                             fontSize: 15,
@@ -44,31 +94,32 @@ class RecentChats extends StatelessWidget {
                         SizedBox(
                           height: 5,
                         ),
-                        Container(
-                          width: MediaQuery.of(context).size.width*0.45,
-                          child: Text(
-                            chat.text,
-                            style: TextStyle(
-                              color: Colors.blueGrey,
-                              fontSize: 15,
-                              fontWeight: FontWeight.w600,
-                            ),
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                        ),
+                        chat.unread
+                            ? Container(
+                                width: 40,
+                                height: 20,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(30),
+                                  color: Theme.of(context).primaryColor,
+                                ),
+                                alignment: Alignment.center,
+                                child: Text(
+                                  'NEW',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              )
+                            : Text(''),
                       ],
-                    ),
+                    )
                   ],
                 ),
-                Column(
-                  children: [
-                    Text(chat.time),
-                    Text('NEW'),
-                  ],
-                )
-              ],
-            );
-          },
+              );
+            },
+          ),
         ),
       ),
     );
