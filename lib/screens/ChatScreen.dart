0414e5c86a850af65ce71f363e36d49d4e0daa62
Alfarobi <alfarobi.dev@gmail.com>
2020-05-28 22:38:34 +0700

    finish

--- lib/screens/ChatScreen.dart
+++ lib/screens/ChatScreen.dart
@@ -13,11 +13,98 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
-  _buildMessage(MessageModel messageModel,bool isMe){
+  _buildMessage(MessageModel messageModel, bool isMe) {
+    final Container container = Container(
+      margin: isMe
+          ? EdgeInsets.only(top: 8, bottom: 8, left: 80)
+          : EdgeInsets.only(top: 8, bottom: 8),
+      padding: EdgeInsets.symmetric(horizontal: 25, vertical: 15),
+      width: MediaQuery.of(context).size.width * 0.75,
+      decoration: BoxDecoration(
+        color: isMe ? Theme.of(context).accentColor : Color(0xFFFFEFEE),
+        borderRadius: isMe
+            ? BorderRadius.only(
+                topLeft: Radius.circular(15),
+                bottomLeft: Radius.circular(15),
+              )
+            : BorderRadius.only(
+                bottomRight: Radius.circular(15),
+                topRight: Radius.circular(15),
+              ),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            messageModel.time,
+            style: TextStyle(
+              color: Colors.blueGrey,
+              fontSize: 16,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          SizedBox(
+            height: 8,
+          ),
+          Text(
+            messageModel.text,
+            style: TextStyle(
+              color: Colors.blueGrey,
+              fontSize: 16,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ],
+      ),
+    );
+
+    if (isMe) {
+      return container;
+    }
+    return Row(
+      children: [
+        container,
+        IconButton(
+          icon: messageModel.isLiked
+              ? Icon(Icons.favorite)
+              : Icon(Icons.favorite_border),
+          iconSize: 30.0,
+          color: messageModel.isLiked
+              ? Theme.of(context).primaryColor
+              : Colors.blueGrey,
+          onPressed: () {},
+        ),
+      ],
+    );
+  }
+
+  _buildMessageComposer() {
     return Container(
-      margin: isMe? EdgeInsets.only(top: 8,bottom: 8, left: 80) : EdgeInsets.only(top: 8,bottom: 8, right: 80),
-      color: Color(0xFFFFEFEE),
-      child: Text(messageModel.text),
+      padding: EdgeInsets.symmetric(horizontal: 8),
+      height: 70,
+      color: Colors.white,
+      child: Row(children: [
+        IconButton(
+          icon: Icon(Icons.photo),
+          iconSize: 25,
+          color: Theme.of(context).primaryColor,
+          onPressed: () {},
+        ),
+        Expanded(
+          child: TextField(
+            textCapitalization: TextCapitalization.sentences,
+            onChanged: (value) {},
+            decoration:
+                InputDecoration.collapsed(hintText: 'Send a message...'),
+          ),
+        ),
+        IconButton(
+          icon: Icon(Icons.send),
+          iconSize: 25,
+          color: Theme.of(context).primaryColor,
+          onPressed: () {},
+        ),
+      ]),
     );
   }
 
@@ -44,34 +131,39 @@ class _ChatScreenState extends State<ChatScreen> {
           ),
         ],
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(30),
-                  topRight: Radius.circular(30),
+      body: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: Column(
+          children: [
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30),
+                  ),
                 ),
-              ),
-              child: ClipRRect(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(30),
-                  topRight: Radius.circular(30),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30),
+                  ),
+                  child: ListView.builder(
+                      reverse: true,
+                      padding: EdgeInsets.only(top: 15),
+                      itemCount: messages.length,
+                      itemBuilder: (BuildContext contex, int index) {
+                        final MessageModel message = messages[index];
+                        final bool isMe = message.sender.id == currentUser.id;
+                        return _buildMessage(message, isMe);
+                      }),
                 ),
-                child: ListView.builder(
-                    padding: EdgeInsets.only(top: 15),
-                    itemCount: messages.length,
-                    itemBuilder: (BuildContext contex, int index) {
-                      final MessageModel message = messages[index];
-                      final bool isMe = message.sender.id == currentUser.id;
-                      return _buildMessage(message, isMe);
-                    }),
               ),
             ),
-          ),
-        ],
+            _buildMessageComposer(),
+          ],
+        ),
       ),
     );
   }
